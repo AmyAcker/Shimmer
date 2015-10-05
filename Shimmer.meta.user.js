ab9e798203d92b6722bb90e74688f4d557813381
AmyAcker <amyacker@tutanota.com>
2015-10-04 20:26:14 -0400

    Create Shimmer.meta.user.js

---
+++ Shimmer.meta.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name          Shimmer
+// @namespace     http://userstyles.org
+// @description	  A nightmode for Skimur!
+// @author        Amy
+// @include       http://skimur.com/*
+// @include       https://skimur.com/*
+// @include       http://*.skimur.com/*
+// @include       https://*.skimur.com/*
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @grant         GM_deleteValue
+// @grant         GM_listValues
+// @grant         GM_openInTab
+// @run-at        document-start
+// @downloadURL   https://github.com/AmyAcker/Shimmer/raw/master/Shimmer.user.js
+// @version       1.0.1
+// ==/UserScript==
